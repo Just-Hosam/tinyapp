85dcea5e42da23302c396d5193eaf5495755dba4
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-02-18 14:25:50 -0700

    installed mocha and chai for testing

--- express_server.js
+++ express_server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const { generateRandomString, urlsForUser, getUserByEmail } = require('./helpers');
